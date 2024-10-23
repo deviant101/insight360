491cbe68d139eda55e6f87152974c3176f48337b
deviant101 <115878714+deviant101@users.noreply.github.com>
2024-10-23 16:38:56 +0500

    header created

---
+++ src/components/Header.jsx
@@ -0,0 +1,48 @@
+// Header.js
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="top-row">
+        <div className="logo">
+          <img src={require('../assets/logo.png')} alt="Megasis Logo" />
+        </div>
+        <div className="header-right">
+          <a href="/signin" className="sign-in">Sign In</a>
+          <div className="social-icons">
+            <a href="https://facebook.com"><i className="fab fa-facebook-f"></i></a>
+            <a href="https://instagram.com"><i className="fab fa-instagram"></i></a>
+            <a href="https://twitter.com"><i className="fab fa-twitter"></i></a>
+            <a href="https://youtube.com"><i className="fab fa-youtube"></i></a>
+            <a href="https://vimeo.com"><i className="fab fa-vimeo-v"></i></a>
+            <a href="https://linkedin.com"><i className="fab fa-linkedin-in"></i></a>
+          </div>
+        </div>
+      </div>
+
+      <div className="bottom-row">
+        <nav className="navigation">
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/page">Page</a></li>
+            <li><a href="/science">Science</a></li>
+            <li><a href="/technology">Technology</a></li>
+            <li><a href="/beauty">Beauty</a></li>
+            <li><a href="/health">Health</a></li>
+            <li><a href="/arts-culture">Arts & Culture</a></li>
+            <li><a href="/opinion">Opinion</a></li>
+            <li><a href="/videos">Videos</a></li>
+            <li><a href="/gallery">Gallery</a></li>
+          </ul>
+        </nav>
+        <div className="search-icon">
+          <i className="fas fa-search"></i>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;

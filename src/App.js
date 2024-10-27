cb392541fc5664a267fe15b5cc87231884421e4f
deviant101 <115878714+deviant101@users.noreply.github.com>
2024-10-27 17:13:29 +0500

    create: newslist

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import Header from './components/Header';
 import MainSection from './components/MainSection';
-import NewsList from './components/NewsList';
+import Footer from './components/Footer';
+import Newsletter from './components/Newsletter';
 import './App.css';
+import NewsList from './components/NewsList';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <MainSection />
-      {/* Other components */}
+      <NewsList />
+      <Newsletter />
+      <Footer />
     </div>
   );
 }
